44e4413a93a215d2a2b88085e91290b98f8ae0cc
蔡子勤 <169122514+A111221027@users.noreply.github.com>
2025-08-06 17:42:25 +0800

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 let pool;
 
@@ -80,6 +83,7 @@ app.listen(3000, async () => {
   await connectToDatabase();
   console.log('Server is running on http://localhost:3000');
 });
+
 
 
 
